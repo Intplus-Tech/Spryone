f6fdd7a8cb4642e5115f146ea0e6a06a4d0cee7c
Dhee-codes <divine.e.obiorah@gmail.com>
2026-02-17 22:21:20 +0100

    Fix custom checkbox ordering. Simplify checkmark toggle display

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -109,12 +109,9 @@ export const ContactForm = () => {
 
             <div className="md:col-span-2">
               <label className="flex items-center gap-3">
-                <input type="checkbox" className="sr-only peer" />
-                <span className="w-4 aspect-square border border-brand rounded-full flex items-center justify-center">
-                  <svg className="opacity-0 peer-checked:opacity-100 text-brand" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" clipRule="evenodd" d="M0 10C0 7.34784 1.05357 4.8043 2.92893 2.92893C4.8043 1.05357 7.34784 0 10 0C12.6522 0 15.1957 1.05357 17.0711 2.92893C18.9464 4.8043 20 7.34784 20 10C20 12.6522 18.9464 15.1957 17.0711 17.0711C15.1957 18.9464 12.6522 20 10 20C7.34784 20 4.8043 18.9464 2.92893 17.0711C1.05357 15.1957 0 12.6522 0 10H0ZM9.42933 14.28L15.1867 7.08267L14.1467 6.25067L9.23733 12.3853L5.76 9.488L4.90667 10.512L9.42933 14.2813V14.28Z" fill="currentColor"/>
-                  </svg>
-                </span>
+                <input
+                  type="checkbox"
+                  className="peer relative appearance-none w-4 aspect-square border rounded-full focus:outline-none checked:border-brand checked:bg-[url(/contact/check-mark.svg)] checked:bg-center" />
                 <span className="text-foreground/60 font-nuni select-none">subscribe to receive the latest news and exclusive offers</span>
               </label>
             </div>
